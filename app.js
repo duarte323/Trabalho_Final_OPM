8bf3e0ae3416234fc3f979f8314d7dcfdce35cca
duarte323 <135663833+duarte323@users.noreply.github.com>
2026-02-03 13:15:18 +0000

    re

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 function setup() {
   createCanvas(1280, 800);
   
-  text('Jogador 1', 500, 500);
+ 
 }
 
 function draw() {
   background(255,255,255);
+
   fill (0,0,0)
    rect(width/2-560 , height/2-320, 1120, 640);
+   
    fill (255)
    rect(width/2-200 , height/2-250, 400, 500);
+
    fill (0)
    rect(width/2-195 , height/2-245, 390,490);
+
+ fill (255)
+rect(width/2-125 , height/2-25, 250,50);
+
+ fill (255)
+rect(width/2-125 , height/2+55, 250,50);
   
-   
+    text('RITMO DO MEDO',width/2-20, height/2-150);
 }
 
