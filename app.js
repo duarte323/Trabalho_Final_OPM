74fac144e7e81d11c0787ac6eedbd2be28d98760
duarte323 <135663833+duarte323@users.noreply.github.com>
2026-02-03 13:06:24 +0000

    y7

---
+++ app.js
@@ -0,0 +1,18 @@
+function setup() {
+  createCanvas(1280, 800);
+  
+  text('Jogador 1', 500, 500);
+}
+
+function draw() {
+  background(255,255,255);
+  fill (0,0,0)
+   rect(width/2-560 , height/2-320, 1120, 640);
+   fill (255)
+   rect(width/2-200 , height/2-250, 400, 500);
+   fill (0)
+   rect(width/2-195 , height/2-245, 390,490);
+  
+   
+}
+
